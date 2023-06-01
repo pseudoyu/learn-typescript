6137b8004361c5277c1b110da75f3eb937a77fe6
pseudoyu <pseudoyu@connect.hku.hk>
2023-06-02 05:18:06 +0800

    feat: add edit user logic and test

--- nestjs-api-demo/src/user/user.controller.ts
+++ nestjs-api-demo/src/user/user.controller.ts
@@ -1,14 +1,23 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Patch, Req, UseGuards } from '@nestjs/common';
 import { JwtGuard } from '../auth/guard/jwt.guard';
 import { Request } from 'express';
 import { GetUser } from '../auth/decorator';
 import { User } from '@prisma/client';
+import { UserService } from './user.service';
+import { EditUserDto } from './dto';
 
 @UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
+  constructor(private userService: UserService) {}
+
   @Get('me')
   getMe(@GetUser() user: User) {
     return user;
+  }
+
+  @Patch()
+  editUser(@GetUser('id') userId: number, @Body() dto: EditUserDto) {
+    return this.userService.editUser(userId, dto);
   }
 }
--- nestjs-api-demo/test/app.e2e-spec.ts
+++ nestjs-api-demo/test/app.e2e-spec.ts
@@ -1,9 +1,14 @@
 import { Test } from '@nestjs/testing';
+import * as pactum from 'pactum';
 import { AppModule } from '../src/app.module';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { PrismaService } from '../src/prisma/prisma.service';
+import { AuthDto } from 'src/auth/dto';
+import { EditUserDto } from 'src/user/dto';
 
 describe('App e2e', () => {
   let app: INestApplication;
+  let prisma: PrismaService;
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
       imports: [AppModule],
@@ -17,11 +22,172 @@ describe('App e2e', () => {
     );
 
     await app.init();
+    await app.listen(3333);
+
+    prisma = app.get(PrismaService);
+    await prisma.cleanDb();
+    pactum.request.setBaseUrl('http://localhost:3333');
   });
 
   afterAll(() => {
     app.close();
   });
 
-  it.todo('should be true');
+  describe('Auth', () => {
+    describe('Signup', () => {
+      it('shoyld throw if email empty', () => {
+        const dto: AuthDto = {
+          email: '',
+          password: 'zy',
+        };
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody(dto)
+          .expectStatus(400);
+      });
+
+      it('should throw if password empty', () => {
+        const dto: AuthDto = {
+          email: 'zy@zy.com',
+          password: '',
+        };
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody(dto)
+          .expectStatus(400);
+      });
+
+      it('should throw if email invalid', () => {
+        const dto: AuthDto = {
+          email: 'zy',
+          password: 'zy',
+        };
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody(dto)
+          .expectStatus(400);
+      });
+
+      it('should create a new user', () => {
+        const dto: AuthDto = {
+          email: 'zy@zy.com',
+          password: 'zy',
+        };
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody(dto)
+          .expectStatus(201);
+      });
+    });
+
+    describe('Login', () => {
+      it('should throw if email empty', () => {
+        const dto: AuthDto = {
+          email: '',
+          password: 'zy',
+        };
+        return pactum
+          .spec()
+          .post('/auth/signin')
+          .withBody(dto)
+          .expectStatus(400);
+      });
+
+      it('should throw if password empty', () => {
+        const dto: AuthDto = {
+          email: 'zy@zy.com',
+          password: '',
+        };
+        return pactum
+          .spec()
+          .post('/auth/signin')
+          .withBody(dto)
+          .expectStatus(400);
+      });
+
+      it('should throw if email invalid', () => {
+        const dto: AuthDto = {
+          email: 'zy',
+          password: 'zy',
+        };
+        return pactum
+          .spec()
+          .post('/auth/signin')
+          .withBody(dto)
+          .expectStatus(400);
+      });
+
+      it('should signin', () => {
+        const dto: AuthDto = {
+          email: 'zy@zy.com',
+          password: 'zy',
+        };
+        return pactum
+          .spec()
+          .post('/auth/signin')
+          .withBody(dto)
+          .expectStatus(200)
+          .stores('userAt', 'access_token');
+      });
+    });
+  });
+
+  describe('Users', () => {
+    describe('Get current user', () => {
+      it('should get current user', () => {
+        return pactum
+          .spec()
+          .get('/users/me')
+          .withHeaders({
+            Authorization: 'Bearer $S{userAt}',
+          })
+          .expectStatus(200);
+      });
+    });
+
+    describe('Update current user', () => {
+      it('should update current user', () => {
+        const dto: EditUserDto = {
+          firstName: 'Yu',
+          lastName: 'Zhang',
+          email: 'test@zy.com',
+        };
+
+        return pactum
+          .spec()
+          .patch('/users')
+          .withHeaders({
+            Authorization: 'Bearer $S{userAt}',
+          })
+          .withBody(dto)
+          .expectStatus(200);
+      });
+    });
+  });
+
+  describe('Bookmarks', () => {
+    describe('Create bookmark', () => {
+      it.todo('should create a new bookmark');
+    });
+
+    describe('Get bookmarks', () => {
+      it.todo('should get all bookmarks');
+    });
+
+    describe('Get bookmark by id', () => {
+      it.todo('should get a bookmark by id');
+    });
+
+    describe('Update bookmark', () => {
+      it.todo('should update a bookmark');
+    });
+
+    describe('Delete bookmark', () => {
+      it.todo('should delete a bookmark');
+    });
+  });
 });
